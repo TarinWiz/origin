10d2a64610a68cff9bf4759f65ac7b36c5cddc41
TarinWiz <82198127+TarinWiz@users.noreply.github.com>
2022-02-12 20:45:17 +0900

    _app.js修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import Layout from "../components/Layout";
+import { theme } from "../utils/themeConfig";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Head from "next/head";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Head>
+        <title>K origin</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MuiThemeProvider>
+  );
+};
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
-import Head from "next/head";
-import Layout from "../components/Layout";
-import { theme } from "../utils/themeConfig";
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
+const Home = () => {
+  return <div>aaa</div>;
+};
 
-export default function Home() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Head>
-          <title>K origin</title>
-        </Head>
-      </Layout>
-    </MuiThemeProvider>
-  );
-}
+export default Home;
